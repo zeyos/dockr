d42241bc325cce4575fb0eee05c08c1a593af580
Peter Haider <peter.haider@zeyos.com>
2025-10-24 11:11:56 +0200

    Fix template.sh

--- lib/template.sh
+++ lib/template.sh
@@ -6,15 +6,17 @@ render_template() {
   local tmp
   tmp="$(mktemp)"
   cp "$template_file" "$tmp"
+  local double_quote='"'
+  local escaped_double_quote='\"'
+
   while [[ $# -gt 0 ]]; do
     local key="$1" value="$2"
     shift 2
     value=${value//\/\\}
     value=${value//&/\&}
-    value=${value//"/\"}
+    value=${value//${double_quote}/${escaped_double_quote}}
     sed -i "s/{{$key}}/$value/g" "$tmp"
   done
   ensure_file_contents "$output_file" "$(cat "$tmp")" 640 root root
   rm -f "$tmp"
 }
-
